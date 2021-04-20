5f92b990b3567c21a81bb29e832e8bd61fdbf13a
congcong <284348736@qq.com>
2021-04-20 11:12:33 +0800

    lc_083

---
+++ 1-100/lc_083.cpp
@@ -0,0 +1,48 @@
+/**
+ * 删除排序链表中的重复元素
+ * 
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ * 返回同样按升序排列的结果链表。
+ * 
+ * 示例 1：
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ */
+#include <iostream>
+#include "../common.h"
+using namespace std;
+
+
+// 0 1 1 2
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head)
+            return head;
+        ListNode *fast = head;
+        while(fast && fast->next) {
+            if(fast->val == fast->next->val) {
+                fast->next = fast->next->next;
+            } else {
+                fast = fast->next;
+            }
+        }
+        return head;
+    }
+};
+
+int main() {
+    ListNode l1(1);
+    ListNode l2(1);
+    ListNode l3(2);
+    l1.next = &l2;
+    l2.next = &l3;
+
+    Solution s;
+    ListNode *l = s.deleteDuplicates(&l1);
+    while(l->next) {
+        cout << l->val << endl;
+        l = l->next;
+    }
+    return 0;
+}
